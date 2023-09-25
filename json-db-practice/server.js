98f891aea74164c98bcc1eb5c78be64a9b45ea6d
deveripon <developershahadathussainripon@gmail.com>
2023-09-25 18:33:25 +0600

    Update : basic server setup done

---
+++ json-db-practice/server.js
@@ -0,0 +1,46 @@
+//load dependencies
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+//Load environment variables
+const PORT = process.env.PORT || 6060;
+//init express
+const app = express();
+//use supporting middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve("public")));
+
+//<!-- ==========  Start From Here ========== -->//
+//<!-- ==========  End From Here ========== -->//
+
+//404 error handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: 404,
+        message: "Page not found",
+    });
+});
+//error handler
+app.use((err, req, res, next) => {
+    if (err.message) {
+        res.status(500).json({
+            status: 500,
+            message: err.message,
+        });
+    } else {
+        res.status(500).json({
+            status: 500,
+            message: "There was an Server side error",
+        });
+    }
+});
+
+//server listen on port
+app.listen(PORT, () => {
+    console.log(` server listening on port ${PORT} `.bgGreen.blue.bold);
+});
